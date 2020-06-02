c67851a6f2317c4f18ce19fb9d4dacabcff500ee
Akande David <adavidoladele@gmail.com>
2020-06-02 01:13:15 +0100

    email and env file created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 
-const url = "mongodb://127.0.0.1:27017/task-manager-api" 
 
-mongoose.connect(url, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser:true, 
     useUnifiedTopology :true,
     useCreateIndex : true,
